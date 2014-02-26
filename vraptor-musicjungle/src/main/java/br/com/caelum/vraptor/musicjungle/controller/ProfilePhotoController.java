9100471565f4529aec085ebac06099c8cd9eae32
Francisco Sokol <chico.sokol@gmail.com>
2014-02-26 20:25:07 -0300

    integrating with vraptor-amazonS3

--- vraptor-musicjungle/src/main/java/br/com/caelum/vraptor/musicjungle/controller/ProfilePhotoController.java
+++ vraptor-musicjungle/src/main/java/br/com/caelum/vraptor/musicjungle/controller/ProfilePhotoController.java
@@ -1,11 +1,14 @@
 package br.com.caelum.vraptor.musicjungle.controller;
 
+import java.net.URL;
+
 import javax.inject.Inject;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.amazonS3.FileStorage;
 import br.com.caelum.vraptor.musicjungle.interceptor.Public;
 import br.com.caelum.vraptor.paperclip.UploadedImage;
 import br.com.caelum.vraptor.paperclip.crop.CenteredCrop;
@@ -13,7 +16,7 @@ import br.com.caelum.vraptor.paperclip.crop.Crop;
 import br.com.caelum.vraptor.paperclip.crop.CropType;
 import br.com.caelum.vraptor.paperclip.crop.ImageCropper;
 import br.com.caelum.vraptor.paperclip.resize.ImageResizer;
-import br.com.caelum.vraptor.paperclip.resize.SimpleResize;
+import br.com.caelum.vraptor.view.Results;
 
 @Controller
 public class ProfilePhotoController {
@@ -26,7 +29,10 @@ public class ProfilePhotoController {
 	
 	@Inject
 	private ImageCropper cropper;
-
+	
+	@Inject
+	private FileStorage storage;
+	
 	@Public
 	@Get("/profile/upload")
 	public void uploadForm() {
@@ -35,11 +41,12 @@ public class ProfilePhotoController {
 	@Public
 	@Post("/profile/upload")
 	public void savePhoto(@Crop(width=100, height=100, type=CropType.TOP_LEFT) UploadedImage image) {
-		image.save("/images/upload.png");
-		resizer.resize(image, new SimpleResize(200, 200)).save("/images/upload2.png");
+		URL saved = image.save("/images/upload.png");
 		
 		cropper.crop(image, new CenteredCrop(image, 50, 50));
 		
-		result.redirectTo(this).uploadForm();
+		result.use(Results.http()).body(saved.toString());
 	}
+	
+	
 }
